8b3021009434adbb767020d18f7d1c036a05da28
Tyler Cook <tylercook@gmail.com>
2010-08-20 21:33:28 -0400

    Added Flixel Project Generator v. 1.0

---
+++ ActionScript 3 Flixel.tmbundle/Templates/Project.tmTemplate/Project.as
@@ -0,0 +1,35 @@
+//AS3///////////////////////////////////////////////////////////////////////////
+// 
+// Copyright ${TM_YEAR} ${TM_ORGANIZATION_NAME:-$TM_FULLNAME}
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+package
+{
+
+import org.flixel.*
+[SWF(width="640", height="480", backgroundColor="#cccccc")] //Set the size and color of the Flash file 
+
+/**
+ * Application entry point for ${TM_NEW_FILE_BASENAME}.
+ * 
+ * @langversion ActionScript 3.0
+ * @playerversion Flash 9.0
+ * 
+ * @author ${TM_FULLNAME}
+ * @since ${TM_DATE}
+ */
+public class ${TM_NEW_FILE_BASENAME} extends FlxGame
+{
+	
+	/**
+	 * @constructor
+	 */
+	public function ${TM_NEW_FILE_BASENAME}()
+	{
+		super();
+	}
+	
+}
+
+}
---
+++ ActionScript 3 Flixel.tmbundle/Templates/Project.tmTemplate/_create_flixel_project.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------
+#
+# New AS3 Project
+# 
+# v3. Switched from copying a directory structure to a flat 
+# Hierarchy as TM doesn't support Template duplication 
+# properly when a directory is present.
+#
+# v4. Removing support for Flex Builder, as Adobe have  
+# changed the underlying format between version 3 + 4.
+# 
+# Also taking the opportunity to use the swfobject as the 
+# embed method as it is much cleaner.  
+#
+# Added ant build file. 
+#
+#-------------------------------------------------------------------------
+
+defaultProjectName="Flixel.tmproj";
+defaultClassPath="org.flixel";
+
+fullProjectPath=$(CocoaDialog filesave \
+			--text "Please name your project and select a folder to save it into" \
+			--title "Create New Project" \
+			--with-extensions .tmproj \
+			--with-file "$defaultProjectName");
+
+if [ -n "$fullProjectPath" ]; then
+	
+	fullProjectPath=$(tail -n1 <<<"$fullProjectPath");
+	projectName=`basename "$fullProjectPath" ".tmproj"`;
+	projectPath=`dirname "$fullProjectPath"`;
+	
+	# Now ask what the class path should be, used to create default dirs.
+	fullClassPath=$(CocoaDialog standard-inputbox \
+				--title "Project Class Path" \
+				--text "$defaultClassPath.$projectName" \
+				--informative-text "Enter the project class path:");
+	
+	if [ -n "$fullClassPath" ]; then
+		classPath=$(tail -n1 <<<"$fullClassPath");
+		classPath=`echo $classPath | tr '.' '/'`;
+		classPathDirectory="$projectPath/$projectName/src/$classPath/";
+		
+		# Create our project directory structure.
+		mkdir -p "$projectPath/$projectName/bin/assets/common/css";
+		mkdir -p "$projectPath/$projectName/bin/assets/common/js";
+		mkdir -p "$projectPath/$projectName/src";
+		
+		# Gather variables to be substituted.
+		TM_NEW_FILE_BASENAME="$projectName";
+		
+		export TM_YEAR=`date "+%Y"`;
+		export TM_DATE=`date "+%d.%m.%Y"`;
+		
+		# Customise file variables for the new project and rename
+		# files to match the project name.
+		perl -pe 's/\$\{([^}]*)\}/$ENV{$1}/g' < "Project.tmproj.xml" > "$projectPath/$projectName/$projectName.tmproj";
+		perl -pe 's/\$\{([^}]*)\}/$ENV{$1}/g' < "Project.as" > "$projectPath/$projectName/src/$projectName.as";
+		perl -pe 's/\$\{([^}]*)\}/$ENV{$1}/g' < "index.html" > "$projectPath/$projectName/bin/index.html";
+		
+		# For the debug html version of the modify the file name.
+		TM_NEW_FILE_BASENAME="$projectName-debug";
+		
+		perl -pe 's/\$\{([^}]*)\}/$ENV{$1}/g' < "index.html" > "$projectPath/$projectName/bin/index-debug.html";
+
+		# Copy static files.		
+		cp "main.css" "$projectPath/$projectName/bin/assets/common/css/main.css";
+		cp "swfaddress.js" "$projectPath/$projectName/bin/assets/common/js/swfaddress.js";
+		cp "swfobject.js" "$projectPath/$projectName/bin/assets/common/js/swfobject.js";
+		
+		# Open the project in TextMate.
+		open -a "TextMate.app" "$projectPath/$projectName/$projectName.tmproj";
+		
+	fi
+
+fi
